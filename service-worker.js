9296d0ad9f29b170003bf8e932a96a4969529dc5
mizunhi <a.gifuni@gmail.com>
2016-11-02 15:18:51 +0100

    test sw

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,31 @@
 var CACHE_NAME = 'gimb-cache-v3';
-var assetBaseUrl = 'https://granze.github.com/playground';
+var assetBaseUrl = 'https://granze.github.io/playground';
 var urlsToCache = [
   assetBaseUrl+'/assets/content-tools.min.js',
   assetBaseUrl+'/assets/content-tools.min.css'
 ];
 
-parent.window.addEventListener('install', function(event) {
+self.addEventListener('install', function(event) {
   // Perform install steps
   console.log('install sw');
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+      caches.open(CACHE_NAME)
+          .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+          })
   );
 });
 
-parent.window.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function(event) {
   console.log('fetch event fired');
   event.respondWith(
-    caches.match(event.request).then(function(cachedResponse) {
-      console.log('rispondo con '+((typeof cachedResponse != 'undefined')?'cache':'nocache'));
-      console.log(caches);
-      console.log(event);
-      return cachedResponse || fetch(event.request);
-    })
+      caches.match(event.request).then(function(cachedResponse) {
+        console.log('rispondo con '+((typeof cachedResponse != 'undefined')?'cache':'nocache'));
+        console.log('cache',caches);
+        console.log('event',event);
+        return cachedResponse || fetch(event.request);
+      })
   );
 });
 
